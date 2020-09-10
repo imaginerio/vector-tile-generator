5c457c083d326d9ab6cc4829f94b240b37af1a14
David Heyman <dave@axismaps.com>
2020-09-10 12:43:51 +0100

    Safely load all files in sequence

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs').promises;
+const { exec } = require('child_process');
 const axios = require('axios');
 const ora = require('ora');
 const { range, omit } = require('lodash');
@@ -8,36 +9,53 @@ const tilelive = require('@mapbox/tilelive');
 const MBTiles = require('@mapbox/mbtiles');
 const s3 = require('@mapbox/tilelive-s3');
 
-const STEP = 50;
+const STEP = 200;
 
 const spinner = ora('Loading layer info').start();
 
+const loadFeatures = async (i, count, step, layer) =>
+  axios
+    .get(
+      `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.id}/query?where=objectid IS NOT NULL&outFields=objectid,nameshort,nameabbrev,name,firstyear,lastyear,type&f=geojson&resultRecordCount=${step}&resultOffset=${i}`
+    )
+    .then(({ data }) => {
+      spinner.text = `${layer.name}: Loading features ${i} / ${count}`;
+      return fs.writeFile(
+        `geojson/${layer.name}-${i}.geojson`,
+        JSON.stringify(omit(data, 'exceededTransferLimit'))
+      );
+    });
+
+const loadLayer = async layer => {
+  spinner.start(`${layer.name}: Loading features`);
+  const {
+    data: { count },
+  } = await axios.get(
+    `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.id}/query?where=objectid IS NOT NULL&f=json&returnCountOnly=true`
+  );
+
+  const step = layer.name === 'GroundCoverPoly' ? 5 : STEP;
+  return range(0, Math.min(count || 1, step), step).reduce(async (previousPromise, next) => {
+    await previousPromise;
+    return loadFeatures(next, count, step, layer);
+  }, Promise.resolve());
+};
+
+exec('rm geojson/*.geojson && rm geojson/final/*.geojson');
 axios
   .get('https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/layers?f=json')
   .then(({ data: { layers } }) => {
     spinner.succeed(`${layers.length} layers loaded`);
-    const layerRequests = layers.map(layer => {
-      spinner.start(`${layer.name}: Loading features`);
-      return axios
-        .get(
-          `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.id}/query?where=objectid IS NOT NULL&f=json&returnCountOnly=true`
-        )
-        .then(({ data: { count } }) => {
-          const featureRequests = range(0, count, STEP).map(i =>
-            axios
-              .get(
-                `https://arcgis.rice.edu/arcgis/rest/services/imagineRio_Data/FeatureServer/${layer.id}/query?where=objectid IS NOT NULL&outFields=objectid,nameshort,nameabbrev,name,firstyear,lastyear,type&f=geojson&resultRecordCount=${STEP}&resultOffset=${i}`
-              )
-              .then(({ data }) => {
-                spinner.text = `${layer.name}: Loading features ${i} / ${count}`;
-                return fs.writeFile(
-                  `geojson/${layer.name}-${i}.geojson`,
-                  JSON.stringify(omit(data, 'exceededTransferLimit'))
-                );
-              })
-          );
-          return Promise.all(featureRequests).then(() => spinner.succeed(`${layer.name} loaded`));
-        });
-    });
-    return Promise.all(layerRequests);
+    return layers
+      .filter(l => !l.name.match(/^ir_rio/))
+      .reduce(async (previousPromise, layer) => {
+        await previousPromise;
+        return loadLayer(layer)
+          .then(() =>
+            mapshaper.runCommands(
+              `-i geojson/${layer.name}*.geojson combine-files -merge-layers -o geojson/final/${layer.name}.geojson force format=geojson`
+            )
+          )
+          .then(() => spinner.succeed(`${layer.name} loaded`));
+      }, Promise.resolve());
   });
